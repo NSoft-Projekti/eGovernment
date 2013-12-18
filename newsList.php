84420ee5bb17d44d50e04395886ad5fa981a5864
Ajda <ajda.maric92@gmail.com>
2013-12-18 21:41:28 +0100

    newsList

--- newsList.php
+++ newsList.php
@@ -1,45 +1,73 @@
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="css/news.css">
-    <meta name="keywords" content="egovernment, gradjani" />
-    <meta name="author" content="Ajda" />
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <title>eGovernment </title>
+    <meta name="description" content="Design Android applications" />
+    <meta name="keywords" content="android, design, technics" />
+    <meta name="author" content="Jelena" />
+    <title>eGovernment :: Home</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="css/DefaultStyle.css.css" rel="stylesheet" type="text/css" />
+    <link href="css/news.css" rel="stylesheet" type="text/css" />
+
 </head>
+
+
 <body>
 <div id="wrapper" >
+
     <div id="header">
+
         <div id="header-up">
+
             <div id="header-logo">
-                <h1 id="header-title">eGovernment</h1>
+                <h1>LOGO STRANICE</h1>
             </div><!--header-logo-->
+
+
             <div id="reg-prijava">
-                <a title="prijava" href="#">Ime i prezime</a>
-                <img id="login" src="img/login-icon.png" >
+
+                <a title="prijava" href="#">Prijava</a>
+
+                <a title="registracija" href="registracija.html">Registracija</a>
+
+
             </div><!--reg-prijava-->
+
+
+
         </div><!--header-up-->
+
         <div id="header-down">
+
             <div id="horizontal-menu">
                 <ul>
-                    <li><a href="Home.php">Home</a> </li>
-                    <li><a href="newsList.php" id="vijesti">Vijesti</a> </li>
-                    <li><a href="suggestionsList.php">Prijedlozi</a> </li>
-                    <li><a href="decisionsList.php">Odluke</a> </li>
-                    <li><a href="usersList.php">Korisnici</a> </li>
+                    <li><a href="#footer">Home</a> </li>
+                    <li><a href="#footer">Vijesti</a> </li>
+                    <li><a href="#footer">Prijedlozi</a> </li>
+                    <li><a href="#footer">Odluke</a> </li>
+                    <li><a href="#footer">Korisnici</a> </li>
+
                 </ul>
+
             </div><!--horizontal-menu-->
+
             <div id="search">
-                <a href="search.php"><div id="img-search">
-                    </div></a><!--img-search-->
-                <input type="text" name="search" >
+                <div id="search-down">
+                    <a href="#"><div id="img-search">
+                        </div></a><!--img-search-->
+
+                    <input type="text" name="search" >
+
+
+                </div>
+
             </div><!--search-->
+
         </div><!--header-down-->
+
+
     </div><!--header--->
     <div id="container">
-        <div id="main">            <!--
-            <div="main-title">
-            <span>Dobrodosli!Jeste li spremni za registraciju na e-Goverment</span>
-            </div><!--main-title-->
+        <div id="main">
             <div id="registration">
                 <form>
                     <!-- end #header -->
@@ -48,12 +76,7 @@
                                 <div id="page-bgbtm">
                                     <div id="content">
                                         <div class="post">
-                                            <?php
-                                            $res = mysql_query('call select_news()');
-                                            if ($res === FALSE) {
-                                            die(mysql_error());}
 
-                                            ?>
                                             <h2 id="title"><a href="newsDetails.php"><!--NASLOV IZ VIJESTI--></a></h2>
                                             <p class="meta"><span class="date"><!--DATUM IZ VIJESTI--></span></br>
                                                 <span class="posted">postavio/la<a href="#"><!--AUTOR IZ VIJESTI--></a></span></p>
@@ -78,23 +101,36 @@
         </div><!--main-->
     </div><!--container-->
     <div id="footer">
+
         <div id="footer-up">
+
             <div id="footer-logo">
-                <img src="Images/logo.png" alt="Logo" title="Logo">
+                <h1>LOGO</h1>
             </div><!--footer-logo-->
+
             <div id="icons">
+
                 <a href="http://www.flickr.com/" target="_blank" > <img title="Flick" src="img/icon-fl.png"></a>
                 <a href="https://twitter.com/" target="_blank">  <img src="img/icon-tw.png"></a>
                 <a href="https://www.facebook.com/" target="_blank"> <img src="img/icon-fb.png"></a>
                 <a href="http://www.google.ba" target="_blank"> <img src="img/icon-gp.png"></a>
                 <a href="http://dribbble.com/" target="_blank"> <img src="img/icon-db.png"></a>
             </div><!--icons-->
+
         </div><!--footer-up-->
+
         <div id="footer-down">
-            <p class="text">Copyright &copy; Tim 4 2013 - <span id="year"></span> Sva prava pridrzana.</p>
+            <p class="text">All design and content Copyright &copy; 2013.<span id="year"></span>. All rights reserved.</p>
         </div><!--footer-down-->
+
+
     </div><!---footer-->
+
+
+
 </div><!--wrapper-->
+
+
 
 </body>
 </html>
