97351f672b2ebfca362b742bcec83a26e0a58e00
Darija007 <darija.bakula@gmail.com>
2013-12-18 13:23:36 +0100

    1

---
+++ phpfile.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP!</title>
+</head>
+<body>
+<?php
+<h1>Dobrodošli na stranicu eGovermenta!</h1>
+    <h2>Unesite svoje podatke...</h2>
+?>
+
+</body>
+</html>
