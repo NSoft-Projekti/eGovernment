f0165b626e1ed7690ad9ae2cc99d0ffede64b484
Ajda <ajda.maric92@gmail.com>
2013-12-20 11:07:52 +0100

    newsList

--- newsList.php
+++ newsList.php
@@ -1,100 +1,143 @@
-<html>
+ <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="css/news.css">
-    <meta name="keywords" content="egovernment, gradjani" />
-    <meta name="author" content="Ajda" />
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <title>eGovernment </title>
+    <meta name="description" content="Design Android applications" />
+    <meta name="keywords" content="android, design, technics" />
+    <meta name="author" content="Jelena" />
+    <title>eGovernment :: Home</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="css/DefaultStyle.css" rel="stylesheet" type="text/css" />
+    <link href="css/postList.css" rel="stylesheet" type="text/css">
+    <meta charset="utf-8">
 </head>
+<?php
+$username = "root";
+$password = "mojapraksa";
+$hostname = "10.0.0.250";
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password)
+or die("Unable to connect to MySQL");
+echo "Connected to MySQL AJDA MAJSTORE";
+?>
+<?php
+//select a database to work with
+$selected = mysql_select_db("tim4",$dbhandle)
+or die("Could not select tim4");
+?>
+
 <body>
 <div id="wrapper" >
+
     <div id="header">
+
         <div id="header-up">
+
             <div id="header-logo">
-                <h1 id="header-title">eGovernment</h1>
+                <h1>LOGO STRANICE</h1>
             </div><!--header-logo-->
+
+
             <div id="reg-prijava">
-                <a title="prijava" href="#">Ime i prezime</a>
-                <img id="login" src="img/login-icon.png" >
+
+                <a title="prijava" href="#">Prijava</a>
+
+                <a title="registracija" href="registracija.html">Registracija</a>
+
+
             </div><!--reg-prijava-->
+
+
+
         </div><!--header-up-->
+
         <div id="header-down">
+
             <div id="horizontal-menu">
                 <ul>
-                    <li><a href="Home.php">Home</a> </li>
-                    <li><a href="newsList.php" id="vijesti">Vijesti</a> </li>
-                    <li><a href="suggestionsList.php">Prijedlozi</a> </li>
-                    <li><a href="decisionsList.php">Odluke</a> </li>
-                    <li><a href="usersList.php">Korisnici</a> </li>
+                    <li><a href="#footer">Home</a> </li>
+                    <li><a href="#footer">Vijesti</a> </li>
+                    <li><a href="#footer">Prijedlozi</a> </li>
+                    <li><a href="#footer">Odluke</a> </li>
+                    <li><a href="#footer">Korisnici</a> </li>
+
                 </ul>
+
             </div><!--horizontal-menu-->
+
             <div id="search">
-                <a href="search.php"><div id="img-search">
+                <div id="search-down">
+                    <a href="#"><div id="img-search">
                     </div></a><!--img-search-->
-                <input type="text" name="search" >
+
+                    <input type="text" name="search" >
+
+
+                </div>
+
             </div><!--search-->
+
         </div><!--header-down-->
+
+
     </div><!--header--->
+
+
     <div id="container">
-        <div id="main">            <!--
-            <div="main-title">
-            <span>Dobrodosli!Jeste li spremni za registraciju na e-Goverment</span>
-            </div><!--main-title-->
-            <div id="registration">
-                <form>
-                    <!-- end #header -->
-                        <div id="page">
-                            <div id="page-bgtop">
-                                <div id="page-bgbtm">
-                                    <div id="content">
-                                        <div class="post">
-                                            <?php
-                                            $res = mysql_query('call select_news()');
-                                            if ($res === FALSE) {
-                                            die(mysql_error());}
+        <div class="post">
+            <?php
+            // I assume this is a specific news item meaning you know it's ONE result
+            $query = "SELECT * FROM post inner join user on post.user_iduser = user.iduser
+             WHERE POST.post_type_idpost_type='1' LIMIT 3"; // so try to use limit 1, no need to add extra steps in the database lookup
 
-                                            ?>
-                                            <h2 id="title"><a href="newsDetails.php"><!--NASLOV IZ VIJESTI--></a></h2>
-                                            <p class="meta"><span class="date"><!--DATUM IZ VIJESTI--></span></br>
-                                                <span class="posted">postavio/la<a href="#"><!--AUTOR IZ VIJESTI--></a></span></p>
-                                            <div class="entry">
-                                                <p>Nalazite se na stranicama <strong>eGovernmenta </strong>, besplatne web platforme za upravljanje udrugom građana.</p>
-                                                <p class="links"><a href="newsDetails.php" class="button">Pročitaj više</a></p>
-                                            </div>
-                                        </div>
-                                        <div class="post post-alt">
-                                            <h2 id="title"><a href="newsDetails.php"><!--NASLOV IZ VIJESTI--></a></h2>
-                                            <p class="meta"><span class="date"><!--DATUM IZ VIJESTI--></span></br>
-                                                <span class="posted">postavio/la<a href="#"><!--AUTOR IZ VIJESTI--></a></span></p>
-                                            <div class="entry">
-                                                <p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. </p>
-                                                <p class="links"><a href="newsDetails.php" class="button">Pročitaj više</a></p>
-                                            </div>
-                                        </div>
+            $result = mysql_query($query);
+            // now loop through the results
+            while($row = mysql_fetch_array($result)){
+            // and use'em however you wish
 
-                        <!-- end #page -->
-                </form>
-            </div><!--registration-->
-        </div><!--main-->
+                   echo '<h2 id="title"><a href="newsDetails.php">'.$row["title"].'</a></h2>';
+                   echo '<p class="meta"><span class="date">'.$row["date_time"].'</span></p>';
+                   echo '<p><span class="posted">postavio/la <a href="#">'.$row["username"].'</a></span></p>';
+                   echo ' <div class="entry"><p>'.$row["summary"].'</p></div>';
+                   echo '<p class="links"><a href="newsDetails.php" class="right">Pročitaj više</a></p></br>';
+            }
+ ?>
+
+        </div>
     </div><!--container-->
+
+    </div><!--container-->
+
     <div id="footer">
+
         <div id="footer-up">
+
             <div id="footer-logo">
-                <img src="Images/logo.png" alt="Logo" title="Logo">
+                <h1>LOGO</h1>
             </div><!--footer-logo-->
+
             <div id="icons">
+
                 <a href="http://www.flickr.com/" target="_blank" > <img title="Flick" src="img/icon-fl.png"></a>
                 <a href="https://twitter.com/" target="_blank">  <img src="img/icon-tw.png"></a>
                 <a href="https://www.facebook.com/" target="_blank"> <img src="img/icon-fb.png"></a>
                 <a href="http://www.google.ba" target="_blank"> <img src="img/icon-gp.png"></a>
                 <a href="http://dribbble.com/" target="_blank"> <img src="img/icon-db.png"></a>
             </div><!--icons-->
+
         </div><!--footer-up-->
+
         <div id="footer-down">
-            <p class="text">Copyright &copy; Tim 4 2013 - <span id="year"></span> Sva prava pridrzana.</p>
+            <p class="text">All design and content Copyright &copy; 2013.<span id="year"></span>. All rights reserved.</p>
         </div><!--footer-down-->
+
+
     </div><!---footer-->
+
+
+
 </div><!--wrapper-->
+
+
 
 </body>
 </html>
