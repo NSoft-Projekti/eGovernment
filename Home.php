dd2fc707656389ae54e1c7cdca3bcb3ee95ae369
Semira <semira.cehic@gmail.com>
2013-12-20 13:46:11 +0100

    Merge branch 'master' of https://github.com/NSoft-Projekti/eGovernment
    Conflicts:
    	css/postList.css

--- Home.php
+++ Home.php
@@ -17,12 +17,12 @@ $password = "mojapraksa";
 $hostname = "10.0.0.250";
 
 //connection to the database
-$dbhandle = mysql_connect($hostname, $username, $password)
+$conn = mysql_connect($hostname, $username, $password)
 or die("Unable to connect to MySQL");
 ?>
 <?php
 //select a database to work with
-$selected = mysql_select_db("tim4",$dbhandle)
+$db = mysql_select_db("tim4",$conn)
 or die("Could not select tim4");
 ?>
 
@@ -40,7 +40,7 @@ or die("Could not select tim4");
 
             <div id="reg-prijava">
 
-                <a title="prijava" href="#">Prijava</a>
+                <a title="prijava" href="login_popup.html">Prijava</a>
 
                 <a title="registracija" href="registracija.html">Registracija</a>
 
@@ -84,18 +84,108 @@ or die("Could not select tim4");
 
     <div id="container">
         <div class="post">
-            <?php
-            $query = "SELECT * FROM post inner join user on post.user_iduser = user.iduser
-             WHERE POST.post_type_idpost_type='1' LIMIT 3";
-            $result = mysql_query($query);
-            while($row = mysql_fetch_array($result)){
 
-            echo '<h2 id="title"><a href="newsDetails.php">'.$row["title"].'</a></h2>';
-            echo '<p class="meta"><span class="date">'.$row["date_time"].'</span></p>';
-            echo '<p><span class="posted">postavio/la <a href="#">'.$row["username"].'</a></span></p>';
-            echo ' <div class="entry"><p>'.$row["summary"].'</p></div>';
-            echo '<p class="links"><a href="newsDetails.php" class="right">Pročitaj više</a></p></br>';
-            }
+            <?php
+
+            // find out how many rows are in the table
+            $sql = "SELECT * FROM post inner join user on post.user_iduser = user.iduser
+             WHERE POST.post_type_idpost_type='1'";
+            $result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
+            $r = mysql_fetch_row($result);
+            $numrows = $r[3];
+
+            // number of rows to show per page
+            $rowsperpage = 3;
+            // find out total pages
+            $totalpages = ceil($numrows / $rowsperpage);
+
+            // get the current page or set a default
+            if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+                // cast var as int
+                $currentpage = (int) $_GET['currentpage'];
+            } else {
+                // default page num
+                $currentpage = 1;
+            } // end if
+
+            // if current page is greater than total pages...
+            if ($currentpage > $totalpages) {
+                // set current page to last page
+                $currentpage = $totalpages;
+            } // end if
+            // if current page is less than first page...
+            if ($currentpage < 1) {
+                // set current page to first page
+                $currentpage = 1;
+            } // end if
+
+            // the offset of the list, based on current page
+            $offset = ($currentpage - 1) * $rowsperpage;
+
+            // get the info from the db
+            $sql = "SELECT * FROM post inner join user on post.user_iduser = user.iduser
+             WHERE POST.post_type_idpost_type='1' LIMIT $offset, $rowsperpage";
+            $result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
+
+            // while there are rows to be fetched...
+            while ($row = mysql_fetch_assoc($result)) {
+                echo '<h2 id="title"><a href="newsDetails.php">'.$row["title"].'</a></h2>';
+                echo '<p class="meta"><span class="date">'.$row["date_time"].'</span></p>';
+                echo '<p><span class="posted">postavio/la <a href="#">'.$row["username"].'</a></span></p>';
+                echo ' <div class="entry"><p>'.$row["summary"].'</p></div>';
+                echo '<p class="links"><a href="newsDetails.php" class="right">Pročitaj više</a></p></br>';
+            } // end while
+
+            /******  build the pagination links ******/
+            // range of num links to show
+            $range = 3;
+
+            // if not on page 1, don't show back links
+            if ($currentpage > 1) {
+                // show << link to go back to page 1
+                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=1'><<</a> ";
+                // get previous page num
+                $prevpage = $currentpage - 1;
+                // show < link to go back to 1 page
+                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$prevpage'><</a> ";
+            } // end if
+
+            // loop to show links to range of pages around current page
+            for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
+                // if it's a valid page number...
+                if (($x > 0) && ($x <= $totalpages)) {
+                    // if we're on current page...
+                    if ($x == $currentpage) {
+                        // 'highlight' it but don't make a link
+                        echo " [<b>$x</b>] ";
+                        // if not current page...
+                    } else {
+                        // make it a link
+                        echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$x'>$x</a> ";
+                    } // end else
+                } // end if
+            } // end for
+
+            // if not on last page, show forward and last page links
+            if ($currentpage != $totalpages) {
+                // get next page
+                $nextpage = $currentpage + 1;
+                // echo forward link for next page
+                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$nextpage'>></a> ";
+                // echo forward link for lastpage
+                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$totalpages'>>></a> ";
+            } // end if
+            /****** end build pagination links ******/
+            /******  build the pagination links ******/
+            // if not on page 1, don't show back links
+            if ($currentpage > 1) {
+                // show << link to go back to page 1
+                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=1'><<</a> ";
+                // get previous page num
+                $prevpage = $currentpage - 1;
+                // show < link to go back to 1 page
+                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$prevpage'><</a> ";
+            } // end if
             ?>
 
         </div>
