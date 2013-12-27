fb615599ea7a5cc407f7613ed4d48729a5a3b569
Ajda <ajda.maric92@gmail.com>
2013-12-27 17:45:38 +0100

    link to login-check

--- login-popup.php
+++ login-popup.php
@@ -44,7 +44,7 @@
 <body>
 <div id="login-box" class="login-popup">
     <a href="#" class="close"><img src="img/close_pop.png" class="btn_close" title="Close Window" alt="Close" /></a>
-    <form method="post" class="signin" action="#">
+    <form method="post" class="signin" action="login-check.php">
         <fieldset class="textbox">
             <label class="username">
                 <span>Username or email</span>
@@ -56,7 +56,7 @@
                 <input id="password" name="password" value="" type="password" placeholder="Password">
             </label>
 
-            <button class="submit button" type="submit">Sign in</button>
+            <button class="submit button" type="submit" >Sign in</button>
 
             <p>
 <!--                <a class="forgot" href="#">Forgot your password?</a>-->
