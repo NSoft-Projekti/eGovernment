5279ec58f8a1b93bfecdaa165fa75b848a74e97b
dajanaGJ <dajanaGJ@users.noreply.github.com>
2014-01-03 15:48:38 +0100

    Added remote connect to connect.php

--- connect.php
+++ connect.php
@@ -6,12 +6,18 @@
  * Time: 11:55
  */
 
+$useRemote = true;
+
 $dbhost = "10.0.0.250"; // ovo je server host
 $dbname = "tim4"; // ime baze
 $dbuser = "root"; // username
 $dbpass = "mojapraksa"; // password
 
- $conn=mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
+if ($useRemote) {
+    $dbhost = "pip.intera.ba:13306";
+}
+
+$conn=mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 $selected=mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
 mysql_query("SET NAMES utf8");
 mysql_query("SET CHARACTER SET utf8");
