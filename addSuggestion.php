e3979a8ef9d848b5fb9323a9c9c47d0708dea95f
Semira <semira.cehic@edu.fit.ba>
2014-01-07 22:26:17 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	default.html
    	style/DefaultStyle.css

---
+++ addSuggestion.php
@@ -0,0 +1,118 @@
+<html>
+<head>
+    <meta name="description" content="Design Android applications" />
+    <meta name="keywords" content="android, design, technics" />
+    <meta name="author" content="Jelena" />
+    <title>eGovernment :: Home</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="style/DefaultStyle.css" rel="stylesheet" type="text/css" />
+    <link href="style/addSuggestion.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8">
+</head>
+
+
+<body>
+<div id="wrapper" >
+
+    <div id="header">
+
+        <div id="header-up">
+
+            <div id="header-logo">
+                <h1>LOGO STRANICE</h1>
+            </div><!--header-logo-->
+
+
+            <div id="reg-prijava">
+
+                <a title="prijava" href="#">Prijava</a>
+
+                <a title="registracija" href="registracija.html">Registracija</a>
+
+
+            </div><!--reg-prijava-->
+
+
+
+        </div><!--header-up-->
+
+        <div id="header-down">
+
+            <div id="horizontal-menu">
+                <ul>
+                    <li><a href="#footer">Home</a> </li>
+                    <li><a href="#footer">Vijesti</a> </li>
+                    <li><a href="#footer">Prijedlozi</a> </li>
+                    <li><a href="#footer">Odluke</a> </li>
+                    <li><a href="#footer">Korisnici</a> </li>
+
+                </ul>
+
+            </div><!--horizontal-menu-->
+
+            <div id="search">
+                <div id="search-down">
+                    <a href="#"><div id="img-search">
+                        </div></a><!--img-search-->
+
+                    <input type="text" name="search" >
+
+
+                </div>
+
+            </div><!--search-->
+
+        </div><!--header-down-->
+
+
+    </div><!--header--->
+
+    <div id="container">
+        </br>
+            <label class="title">Naziv  prijedloga: </label> </br></br>
+        <input class="input" type="text" name="titlePrijedlog" id="titlePrijedlog"/>
+        </br></br>
+        <label class="title">Unesite sadržaj prijedloga: </label> </br></br>
+        <textarea name="contentPrijedlog" id="contentPrijedlog"></textarea>
+
+        </br> </br>
+
+        <hr>
+        <button id="button" name="submit" value="submit">Pošalji</button>
+
+    </div><!--container-->
+
+    <div id="footer">
+
+        <div id="footer-up">
+
+            <div id="footer-logo">
+                <h1>LOGO</h1>
+            </div><!--footer-logo-->
+
+            <div id="icons">
+
+                <a href="http://www.flickr.com/" target="_blank" > <img title="Flick" src="img/icon-fl.png"></a>
+                <a href="https://twitter.com/" target="_blank">  <img src="img/icon-tw.png"></a>
+                <a href="https://www.facebook.com/" target="_blank"> <img src="img/icon-fb.png"></a>
+                <a href="http://www.google.ba" target="_blank"> <img src="img/icon-gp.png"></a>
+                <a href="http://dribbble.com/" target="_blank"> <img src="img/icon-db.png"></a>
+            </div><!--icons-->
+
+        </div><!--footer-up-->
+
+        <div id="footer-down">
+            <p class="text">All design and content Copyright &copy; 2013.<span id="year"></span>. All rights reserved.</p>
+        </div><!--footer-down-->
+
+
+    </div><!---footer-->
+
+</div><!--wrapper-->
+
+
+
+
+
+</body>
+</html>
