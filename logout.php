f87f9e0dac72f4126d9e11890209441b90a27a14
Ajda <ajda.maric92@gmail.com>
2014-01-09 20:27:30 +0100

    završen logout

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // start the same session
 session_destroy(); // kill the session
-header("location:home.php"); // redirecting to home.php after logging out
+header("location: home.php"); // redirecting to home.php after logging out
 exit();
 ?>
