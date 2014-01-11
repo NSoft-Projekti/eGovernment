97bb607eac973384f8efd050f4d01b1627858078
Nenad Tomić <nenad.tomic9@gmail.com>
2014-01-11 21:01:32 +0100

    update

--- addNewsStore.php
+++ addNewsStore.php
@@ -7,14 +7,20 @@
  */
 require("connect.php");
 echo ("Pozvan addNewsStore.php");
+session_start();
 
     echo ("If uslov");
     $inputTitle=mysql_real_escape_string($_POST['title']);
     $inputContent=mysql_real_escape_string($_POST['content']);
     $inputSummary=mysql_real_escape_string($_POST['summary']);
+    $date_time=date("Y-m-d H:i:s");
+    $iduser=mysql_real_escape_string($_SESSION['SESS_MEMBER_ID']);
+
     echo ("Izvrsena pohrana u varijable");
-    mysql_query("INSERT INTO post (title, content, summary, idpost_type, iduser)
-    VALUES ('".$inputTitle."', '".$inputContent."','".$inputSummary."', 1, 10)");
+    mysql_query("INSERT INTO post (title, content,date_time ,summary, idpost_type, iduser)
+    VALUES ('".$inputTitle."', '".$inputContent."','".$date_time."','".$inputSummary."', 1,'".$iduser."')");
+
+    header("location: newsList.php");
 echo mysql_error();
 
 
