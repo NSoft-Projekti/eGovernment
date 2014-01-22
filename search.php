fcf10d1be4ddae0e9dc5474580e1c28620573b9e
Vanja <vanja88.mo@gmail.com>
2014-01-22 12:16:16 +0100

    Update search.php

--- search.php
+++ search.php
@@ -1,18 +1,20 @@
+//
 <html>
 <head>
     <meta name="description" content="Design Android applications" />
     <meta name="keywords" content="android, design, technics" />
+    <meta charset="UTF-8">
     <meta name="author" content="Jelena" />
     <title>eGovernment :: Home</title>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href="style/DefaultStyle.css" rel="stylesheet" type="text/css" />
-    <link href="style/postList.css" rel="stylesheet" type="text/css">
-    <meta charset="utf-8">
+    <link href="style/search.css" rel="stylesheet" type="text/css">
+
 </head>
 
 <?php
 include('connect.php');
-session_start();
+
 ?>
 
 <body>
@@ -23,18 +25,16 @@ session_start();
         <div id="header-up">
 
             <div id="header-logo">
-                <a href="index.php"><img src="img/logo.png"></a>
+                <h1>LOGO STRANICE</h1>
             </div><!--header-logo-->
 
 
             <div id="reg-prijava">
-                <?php
 
-                echo '<a title="prijava" href="profile.php">'.$_SESSION["SESS_FIRST_NAME"].'</a>';
-                echo '<img class="logo" src="img/login-icon.png">';
-                echo '</br>';
-                echo'<a title="odjava" href="logout.php">Odjava</a>';
-                ?>
+                <a title="prijava" href="#">Prijava</a>
+
+                <a title="registracija" href="registracija.html">Registracija</a>
+
 
             </div><!--reg-prijava-->
 
@@ -46,19 +46,20 @@ session_start();
 
             <div id="horizontal-menu">
                 <ul>
-                    <li><a href="index.php">Home</a> </li>
-                    <li><a href="newsList.php">Vijesti</a> </li>
-                    <li><a href="suggestionList.php" class="currentTab">Prijedlozi</a> </li>
-                    <li><a href="decisionList.php">Odluke</a> </li>
+                    <li><a href="home.php">Home</a> </li>
+                    <li><a href="#footer">Vijesti</a> </li>
+                    <li><a href="#footer">Prijedlozi</a> </li>
+                    <li><a href="#footer">Odluke</a> </li>
                     <li><a href="#footer">Korisnici</a> </li>
 
                 </ul>
 
             </div><!--horizontal-menu-->
 
+
             <div id="search">
                 <div id="search-down">
-                    <a href="search.php"><div id="img-search">
+                    <a href="#"><div id="img-search">
                         </div></a><!--img-search-->
 
                     <input type="text" name="search" >
@@ -75,151 +76,91 @@ session_start();
 
 
     <div id="container">
-        <div class="post">
+        <div id="searching">
 
             <?php
 
-            // find out how many rows are in the table
-            $sql = "SELECT * FROM post inner join user on post.iduser = user.iduser
-             WHERE POST.idpost_type='3'";
-            $result = mysql_query($sql);
-            $r = mysql_num_rows($result);
+            error_reporting(E_ALL);
+            ini_set('display_errors', '1');
+            $search_output = "";
+            if(isset($_POST['searchquery']) && $_POST['searchquery'] != ""){
+            $searchquery = preg_replace('#[^a-z 0-9?!]#i', '', $_POST['searchquery']);
 
-            // number of rows to show per page
-            $rowsperpage = 3;
-            // find out total pages
-            $totalpages = ceil($r / $rowsperpage);
+                $searchquery=$_POST['searchquery'];
+                $sqlCommand=("SELECT * FROM post WHERE (`title` LIKE '%$searchquery%' ) OR (`content` LIKE '%$searchquery%' )");
+                $query = mysql_query($sqlCommand) or die(mysql_error());
+                $count = mysql_num_rows($query);
+                if($count > 1){
+                    $search_output .= "<hr />$count results for <strong>$searchquery</strong><hr />$sqlCommand<hr />";
+                    while($row = mysql_fetch_array($query)){
+
+                        $idpost = $row["idpost"];
+                        $title = $row["title"];
+                        $search_output .= "$idpost - <a href='newsDetails.php?id=$idpost'>$title</a><br />";
+
+                    } // close while
+                } else {
+                    $search_output = "<hr />0 results for <strong> $searchquery</strong><hr />$sqlCommand";
+                }
+            }
 
 
-            // get the current page or set a default
-            if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
-                // cast var as int
-                $currentpage = (int) $_GET['currentpage'];
-            } else {
-                // default page num
-                $currentpage = 1;
-            } // end if
 
-            // if current page is greater than total pages...
-            if ($currentpage > $totalpages) {
-                // set current page to last page
-                $currentpage = $totalpages;
-            } // end if
-            // if current page is less than first page...
-            if ($currentpage < 1) {
-                // set current page to first page
-                $currentpage = 1;
-            } // end if
-
-            // the offset of the list, based on current page
-            $offset = ($currentpage - 1) * $rowsperpage;
-
-            // get the info from the db
-            $sql = "SELECT * FROM post inner join user on post.iduser = user.iduser
-             WHERE POST.idpost_type='3' LIMIT $offset, $rowsperpage";
-            $result = mysql_query($sql, $conn) or trigger_error("SQL", E_USER_ERROR);
-
-            // while there are rows to be fetched...
-            while ($row = mysql_fetch_assoc($result)) {
-                $idpost=$row['idpost'];
-                echo '<h2 id="title"><a href="suggestionDetails.php?id='.$idpost.'">'.$row["title"].'</a></h2>';
-                echo '<p class="meta"><span class="date">'.$row["date_time"].'</span></p>';
-                echo '<p><span class="posted">postavio/la <a href="#">'.$row["username"].'</a></span></p>';
-                echo ' <div class="entry"><p>'.$row["summary"].'</p></div>';
-                echo '<p class="links"><a href="suggestionDetails.php?id='.$idpost.'" class="right">Pročitaj više</a></p></br>';
-            } // end while
-
-            /******  build the pagination links ******/
-            // range of num links to show
-            $range = 3;
-
-            // if not on page 1, don't show back links
-            if ($currentpage > 1) {
-                // show << link to go back to page 1
-                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=1'><<</a> ";
-                // get previous page num
-                $prevpage = $currentpage - 1;
-                // show < link to go back to 1 page
-                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$prevpage'><</a> ";
-            } // end if
-
-            // loop to show links to range of pages around current page
-            for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
-                // if it's a valid page number...
-                if (($x > 0) && ($x <= $totalpages)) {
-                    // if we're on current page...
-                    if ($x == $currentpage) {
-                        // 'highlight' it but don't make a link
-                        echo " [<b>$x</b>] ";
-                        // if not current page...
-                    } else {
-                        // make it a link
-                        echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$x'>$x</a> ";
-                    } // end else
-                } // end if
-            } // end for
-
-            // if not on last page, show forward and last page links
-            if ($currentpage != $totalpages) {
-                // get next page
-                $nextpage = $currentpage + 1;
-                // echo forward link for next page
-                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$nextpage'>></a> ";
-                // echo forward link for lastpage
-                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$totalpages'>>></a> ";
-            } // end if
-            /****** end build pagination links ******/
-            /******  build the pagination links ******/
-            // if not on page 1, don't show back links
-            if ($currentpage > 1) {
-                // show << link to go back to page 1
-                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=1'><<</a> ";
-                // get previous page num
-                $prevpage = $currentpage - 1;
-                // show < link to go back to 1 page
-                echo " <a href='{$_SERVER['PHP_SELF']}?currentpage=$prevpage'><</a> ";
-            } // end if
             ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                Search For:
+                <input name="searchquery" type="text" size="44" maxlength="88">
+                Within:
+                <select name="filter1">
+                    <option value="title">Naslov</option>
+                    <option value="content">Sadrzaj</option>
+
+                </select>
+                <input name="myBtn" type="submit">
+                <br />
+            </form>
+            <div>
+                <?php echo $search_output; ?>
+            </div>
+
 
         </div>
 
 
-
     </div><!--container-->
 
-</div><!--container-->
+    <div id="footer">
 
-<div id="footer">
+        <div id="footer-up">
 
-    <div id="footer-up">
+            <div id="footer-logo">
+                <h1>LOGO</h1>
+            </div><!--footer-logo-->
 
-        <div id="footer-logo">
-            <a href="index.php"><img src="img/logo.png"></a>
-        </div><!--footer-logo-->
+            <div id="icons">
 
-        <div id="icons">
+                <a href="http://www.flickr.com/" target="_blank" > <img title="Flick" src="img/icon-fl.png"></a>
+                <a href="https://twitter.com/" target="_blank">  <img src="img/icon-tw.png"></a>
+                <a href="https://www.facebook.com/" target="_blank"> <img src="img/icon-fb.png"></a>
+                <a href="http://www.google.ba" target="_blank"> <img src="img/icon-gp.png"></a>
+                <a href="http://dribbble.com/" target="_blank"> <img src="img/icon-db.png"></a>
+            </div><!--icons-->
 
-            <a href="http://www.flickr.com/" target="_blank" > <img title="Flick" src="img/icon-fl.png"></a>
-            <a href="https://twitter.com/" target="_blank">  <img src="img/icon-tw.png"></a>
-            <a href="https://www.facebook.com/" target="_blank"> <img src="img/icon-fb.png"></a>
-            <a href="http://www.google.ba" target="_blank"> <img src="img/icon-gp.png"></a>
-            <a href="http://dribbble.com/" target="_blank"> <img src="img/icon-db.png"></a>
-        </div><!--icons-->
+        </div><!--footer-up-->
 
-    </div><!--footer-up-->
-
-    <div id="footer-down">
-        <p class="text">All design and content Copyright &copy; 2013.<span id="year"></span>. All rights reserved.</p>
-    </div><!--footer-down-->
+        <div id="footer-down">
+            <p class="text">All design and content Copyright &copy; 2013.<span id="year"></span>. All rights reserved.</p>
+        </div><!--footer-down-->
 
 
-</div><!---footer-->
-
-
+    </div><!---footer-->
 
 </div><!--wrapper-->
 
 
 
+
+
 </body>
 </html>
+
