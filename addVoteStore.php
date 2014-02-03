44e43cb72325a59c76dc8a80cf15fad6a315988f
Nenad <nenad.tomic9@gmail.com>
2014-02-03 13:56:59 +0100

    vote completed

--- addVoteStore.php
+++ addVoteStore.php
@@ -2,12 +2,12 @@
 require("connect.php");
 session_start();
 
-    //iduser = mysql_real_escape_string($_SESSION['SESS_MEMBER_ID']);//** gets iduser using session */
-    $idpoststring = mysql_real_escape_string( $_GET['id']);//** gets idpost using URL */
-    $idpost = (int) $idpoststring;
-    mysql_query("INSERT INTO vote (votevalue, iduser, idpost)
-    VALUES (1,39, 33)");
+$iduser = mysql_real_escape_string($_SESSION['SESS_MEMBER_ID']);//** gets iduser using session */
+$idpost= mysql_real_escape_string( $_GET['id']);//** gets idpost using URL */
+// $idpost = (int) $idpoststring;
+mysql_query("INSERT INTO vote (votevalue, iduser, idpost)
+    VALUES (1,'".$iduser."','".$idpost."')");
 echo mysql_error();
-    //header("location: suggestionList.php");
+header("location: suggestionList.php");
 
 ?>
