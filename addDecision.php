779b94f021191a1732d1637f54b60ced87704136
Darija007 <Darija007@users.noreply.github.com>
2014-03-04 01:25:04 +0100

    added dropdown menu

--- addDecision.php
+++ addDecision.php
@@ -5,13 +5,13 @@
     <title>eGovernment :: Home</title>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href="style/DefaultStyle.css" rel="stylesheet" type="text/css" />
-    <link href="style/postList.css" rel="stylesheet" type="text/css" />
     <link href="style/addNews.css" rel="stylesheet" type="text/css" />
+    <link href="style/profile.css" rel="stylesheet" type="text/css" />
 
     <meta charset="utf-8">
 </head>
-<?php
 
+<?php
 include('connect.php');
 session_start();
 if(!isset ($_SESSION['SESS_MEMBER_ID']))
@@ -45,26 +45,21 @@ $idsubcategory = $_GET['id'];
 
                     //checking gender and displaying matching picture
                     if($gender=='M'){
-                        echo '<a title="prijava" href="profile.php">'.$_SESSION["SESS_FIRST_NAME"].'</a>';
+                        echo '<a href="profile.php">'.$_SESSION["SESS_FIRST_NAME"].'</a>';
                         echo '<img class="logo" src="img/men.png">';
                         echo '</br>';
-                        echo'<a title="odjava" href="logout.php">Odjava</a>';
+                        echo'<a href="logout.php">Odjava</a>';
                     }
 
                     //if it's not male gender, it displays female image
                     else {
-                        echo '<a title="prijava" href="profile.php">'.$_SESSION["SESS_FIRST_NAME"].'</a>';
+                        echo '<a href="profile.php">'.$_SESSION["SESS_FIRST_NAME"].'</a>';
                         echo '<img class="logo" src="img/girl.png">';
                         echo '</br>';
-                        echo'<a title="odjava" href="logout.php">Odjava</a>';
+                        echo'<a href="logout.php">Odjava</a>';
                     }
                 }
 
-                //includes login popup form
-                else {
-                    include_once("loginPopup.php");
-                    echo'<a title="registracija" href="registration.php">Registracija</a>';
-                }
                 ?>
             </div><!--reg-prijava-->
 
@@ -74,21 +69,58 @@ $idsubcategory = $_GET['id'];
 
         <div id="header-down">
 
-            <div id="horizontal-menu">
+            <nav>
                 <ul>
                     <li><a href="index.php">Home</a> </li>
-                    <li><a href="newsList.php" class="currentTab">Vijesti</a> </li>
-                    <li><a href="suggestionList.php">Prijedlozi</a> </li>
-                    <li><a href="decisionList.php">Odluke</a> </li>
-                    <li><a href="#footer">Korisnici</a> </li>
+                    <li><a href="newsList.php">Vijesti</a> </li>
+                    <?php
+
+                    if(isset ($_SESSION['SESS_MEMBER_ID'])){
+                        echo'<li><a href="suggestionList.php">Prijedlozi</a>';
+                        echo'<ul>';
+
+                        $sqlCat = "SELECT idcategory, name FROM category WHERE idcategory != '1'";
+                        $resultCat=mysql_query($sqlCat, $conn);
+                        while($rowCat = mysql_fetch_assoc($resultCat)){
+                            echo '<li>';
+                            echo '<a href="suggestionList.php?id='.$rowCat['idcategory'].'">'.$rowCat["name"].'</a>';
+                            echo '<ul>';
+
+                            $idcategory = $rowCat['idcategory'];
+                            $sqlSub = "SELECT idcategory, name FROM subcategory WHERE idcategory = $idcategory";
+                            $resSub=mysql_query($sqlSub, $conn);
+                            while($rowSub =mysql_fetch_assoc ($resSub)){
+                                echo '<li>';
+                                echo '<a href="suggestionList.php?id='.$rowSub['idcategory'].'">'.$rowSub["name"].'</a>';
+                                echo '</li>';
+                            }
+                            echo '<li>';
+                            echo '<a href="addSubcategory.php">+ Nova potkategorija</a>';
+                            echo '</li>';
+                            echo '<li>';
+                            echo '<a href="addSuggestion.php">+ Novi prijedlog</a>';
+                            echo '</li>';
+
+                            echo '</ul>';
+
+                            echo '</li>';}
+                        ?>
+
+                        <?php echo'</ul>';?>
+                        <?php echo'</li>';?>
+
+                        <li><a href="decisionList.php">Odluke</a></li>
+                        <li><a href="userList.php">Korisnici</a></li>
+                    <?php }
+                    ?>
 
                 </ul>
 
-            </div><!--horizontal-menu-->
+            </nav><!--horizontal-menu-->
 
             <div id="search">
                 <div id="search-down">
-                    <a href="#"><div id="img-search">
+                    <a href="search.php?id=<?php $string ?>"><div id="img-search">
                         </div></a><!--img-search-->
 
                     <input type="text" name="search" >
@@ -139,7 +171,7 @@ $idsubcategory = $_GET['id'];
                 echo ' <div class="entry"><p>'.$row4["content"].'</p></div>';
                 echo '<p >postavio/la <a href="userProfile.php?id='.$row4['iduser'].'" class="user_link">'.$row4['username'].'</a></p></br>';
                 echo '<p class="links"><a href="suggestionDetails.php?id='.$idpost.'" class="right">Pročitaj više</a></p></br>';
-                }
+            }
             else
                 echo 'Nema nista izglasano!';
         }
@@ -160,7 +192,7 @@ $idsubcategory = $_GET['id'];
             </div>
 
             <hr>
-            <input type="submit" name="button" value="Donesi odluku" class="button" />
+            <input class="button" type="submit" name="button" value="Donesi odluku" />
             </form>
 
 
@@ -191,7 +223,7 @@ $idsubcategory = $_GET['id'];
         </div><!--footer-up-->
 
         <div id="footer-down">
-            <p class="text">All design and content Copyright &copy; <span><?php echo date('Y');?></span>. All rights reserved.</p>
+            <p class="text">All design and content Copyright &copy; 2014.<span id="year"></span>. All rights reserved.</p>
         </div><!--footer-down-->
 
 
@@ -203,5 +235,4 @@ $idsubcategory = $_GET['id'];
 
 
 </body>
-
 </html>
