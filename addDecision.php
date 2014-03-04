aac2b26c7e39965ae3ff1af88cda8676d91bacc1
Darija007 <Darija007@users.noreply.github.com>
2014-03-04 01:26:54 +0100

    date

--- addDecision.php
+++ addDecision.php
@@ -223,7 +223,7 @@ $idsubcategory = $_GET['id'];
         </div><!--footer-up-->
 
         <div id="footer-down">
-            <p class="text">All design and content Copyright &copy; 2014.<span id="year"></span>. All rights reserved.</p>
+                <p class="text">All design and content Copyright &copy; <span><?php echo date('Y');?></span>. All rights reserved.</p>
         </div><!--footer-down-->
 
 
